6bb47002cdedef0dca0eee7a02165118f359cc80
AARYA RAJOJU <aaryarajoju@gmail.com>
2021-01-11 16:00:48 +0530

    kinda completed the program

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const Discord = require('discord.js');
 const {prefix, token} = require('./config.json')
 const client = new Discord.Client();
 
+var headsCounter = 0;
+var tailsCounter = 0;
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -15,28 +18,54 @@ client.on('message', (receivedMessage) => {
 });
 
 function processCommand(receivedMessage){
+
     let fullCommand = receivedMessage.content.substr(prefix.length);
     let splitCommand = fullCommand.split(" ");
     let primaryCommand = splitCommand[0];
-    let arguements = splitCommand.slice(1);
+    let arguments = splitCommand.slice(1);
 
-    if (primaryCommand.toLowerCase() == 'ht'){
-        headsOrTalesCommand(receivedMessage, arguements);
-    } 
+    if (primaryCommand.toLowerCase() == 'flip'){
+        someCommand(receivedMessage, arguments);
+    }
 }
 
-function headsOrTalesCommand(receivedMessage, arguements){
-
-    let userSelection = arguements[0].toLowerCase()
-    let result = randomSelect();
-
-    receivedMessage.channel.send('The result of a coin flip is ' + result);
-
-    if (result == userSelection){
-        receivedMessage.channel.send('Congratulations! You\'ve won');
-    } else {
-        receivedMessage.channel.send('Sorry, you didn\'t win');
+function someCommand(receivedMessage, arguments){
+    if (arguments.length > 1) {
+        receivedMessage.channel.send('Number of arguements too many');
+        return;
     }
+
+    if (arguments.length == 0){
+        flipOnceCommand(receivedMessage);
+    } else if (arguments.length == 1){
+        let numOfTimes = arguments[0];
+        flipMultipleCommand(receivedMessage, numOfTimes);
+    } else {
+        receivedMessage.channel.send('ERROR!');
+    }
+}
+
+function flipOnceCommand(receivedMessage){
+
+    result = randomSelect();
+    receivedMessage.channel.send('The result of 1 flip is  **' + result + "**");
+    tailsCounter = 0;
+    headsCounter = 0;
+}
+
+function flipMultipleCommand(receivedMessage, numOfTimes){
+    
+    var i;
+    var result;
+
+    for (i = 1; i <= numOfTimes; i++){
+        result = randomSelect();
+        receivedMessage.channel.send(i + '.  **' + result + '**');
+    }
+    receivedMessage.channel.send('number of TAILS:  **' + tailsCounter + '**');
+    receivedMessage.channel.send('number of HEADS:  **' + headsCounter + '**');
+    tailsCounter = 0;
+    headsCounter = 0;
 }
 
 function randomSelect(){
@@ -44,11 +73,13 @@ function randomSelect(){
 
     switch (randomSelectionNum){
         case 1: 
-            randomSelection = 'tails';
+            randomSelection = 'TAILS';
+            tailsCounter++;
             break;
         
         case 2:
-            randomSelection = 'heads';
+            randomSelection = 'HEADS';
+            headsCounter++;
             break;
     }
     return randomSelection;
